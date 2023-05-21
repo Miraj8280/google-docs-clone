f0f8fe2bdabd38c835923a331424b10a30970d6f
Miraj Asraf <mirajasraf786@gmail.com>
2023-05-21 16:10:19 +0530

    Connected to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,3 +1,21 @@
+const mongoose = require("mongoose");
+const Document = require("./Document");
+
+mongoose.connect("mongodb+srv://mirajasraf786:miraj123@cluster0.khqkhwd.mongodb.net/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB Atlas");
+});
+
 const io = require("socket.io")(3001, {
   cors: {
     origin: "http://localhost:3000",
@@ -5,8 +23,30 @@ const io = require("socket.io")(3001, {
   },
 });
 
-io.on("connection", socket => {
-    socket.on("send-changes", delta => {
-        socket.broadcast.emit("receive-changes", delta);
+const defaultValue = "";
+
+io.on("connection", (socket) => {
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
     });
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
 });
+
+async function findOrCreateDocument(id) {
+  if (id == null) {
+    return;
+  }
+
+  const document = await Document.findById(id);
+  if (document) {
+    return document;
+  }
+  return await Document.create({ _id: id, data: defaultValue });
+}
